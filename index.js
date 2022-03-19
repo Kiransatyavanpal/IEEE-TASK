51ae032a011f1b6bca4cba062689cfcef93fcf95
kiran <kiransatyavanpal@gmail.com>
2022-03-19 11:50:08 +0530

    project done

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+require("dotenv").config()  
 var express = require("express")
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
 
 const app = express()
-
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
+const path = require("path")
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json())
+//app.use(express.static('public'))
+app.use(express.urlencoded({
     extended:true
 }))
-
-mongoose.connect('mongodb+srv://KiranPal:Pranita1910@cluster0.4oaqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+app.set("view-engine","ejs")
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}).then(() => console.log("DB connected"))
+.catch(error => console.log(error));
 
 var db = mongoose.connection;
 
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database"))
+// db.on('error',()=>console.log("Error in Connecting to Database"));
+// db.once('open',()=>console.log("Connected to Database"))
 
 app.post("/sign_up",(req,res)=>{
     var name = req.body.name;
@@ -38,18 +41,22 @@ app.post("/sign_up",(req,res)=>{
             throw err;
         }
         console.log("Record Inserted Successfully");
+        console.log(err);
     });
 
-    return res.redirect('signup_success.html')
+    return res.render('signup_success.ejs')
 
 })
 
 
 app.get("/",(req,res)=>{
-    res.set({
-        "Allow-access-Allow-Origin": '*'
-    })
-    return res.redirect('index.html');
+    // res.set({
+    //     "Allow-access-Allow-Origin": '*'
+    // })
+   res.render('index.ejs');
+});
+app.listen(3000,()=>{
+    console.log("Listening on PORT 3000")
 });
 
 
